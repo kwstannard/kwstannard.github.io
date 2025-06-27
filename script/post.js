96bbf9cbbbbcfb06b7930deddef9469de92546fe
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-06-27 11:46:16 -0400

    checkin

--- script/post.js
+++ script/post.js
@@ -1,7 +1,7 @@
 var dir = 1
 var scale = 1
 fun = function() {
-  Object.values(document.getElementsByTagName("p")).forEach(function(par) {
+  Object.values(document.querySelectorAll("p,pre")).forEach(function(par) {
     par.style.setProperty("transform", "rotate("+scale*dir+"deg)")
     dir = -1 * dir
     scale = scale - 0.05
