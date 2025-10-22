f8a1e30ae6e0e5c5639a48a05a5264a4eabf9c56
Kelly Wolf Stannard <kwstannard@gmail.com>
2025-10-21 20:37:20 -0400

    add toggle button
    type: add

--- script/post.js
+++ script/post.js
@@ -1,5 +1,14 @@
 var dir = 1
 var scale = 1
+
+checkfun = function(element) {
+  if (element.checked) {
+    fun()
+  } else {
+    unfun()
+  }
+}
+
 fun = function() {
   Object.values(document.querySelectorAll("p,pre")).forEach(function(par) {
     par.style.setProperty("transform", "rotate("+scale*dir+"deg)")
@@ -7,6 +16,12 @@ fun = function() {
     scale = scale - 0.05
   })
 };
+
+unfun = function() {
+  Object.values(document.querySelectorAll("p,pre")).forEach(function(par) {
+    par.style.setProperty("transform", "")
+  })
+}
 
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", fun)
